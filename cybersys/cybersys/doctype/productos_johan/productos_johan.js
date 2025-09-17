82baa5cb92d1f4144723ab4ab57ae00e2f09a9e1
Ubuntu <frappe@CyberSys-ERP.53mfzbo4q4aejkhelthjahhfkg.xx.internal.cloudapp.net>
2025-09-17 16:47:24 -0500

    doctype productos johan

---
+++ cybersys/cybersys/doctype/productos_johan/productos_johan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, CyberSys and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Productos Johan", {
+// 	refresh(frm) {
+
+// 	},
+// });
